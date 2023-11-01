ddbaa61f8e21e7975378be12fdb36c1046d8a3b9
asuf29 <asuf29@icloud.com>
2023-11-01 19:36:58 +0300

    learn component structure

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  // function sayHello() {
+  //   console.log("Hello");
+  // }
+
+  const sayHi = (label) => {
+    console.log("Hi " + label);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text style={{ color: "#fff" }}>Hello World!</Text>
+      <Text style={{ color: "#fff" }}>Hello React Native!</Text>
+      <Button
+        title="Press Me!"
+        // onPress={() => {
+        //   console.log("Hello");
+        //   alert("HELLO");
+        // }}
+        onPress={() => sayHi("Asuf")}
+        disabled={false}
+        color="darkred"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +32,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
   },
