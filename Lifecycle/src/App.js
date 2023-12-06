5be0cef40ba2ab223bd42b29c5d4a667b95a3961
asuf29 <asuf29@icloud.com>
2023-12-06 21:23:55 +0300

    Lifecycle clean up

--- Lifecycle/src/App.js
+++ Lifecycle/src/App.js
@@ -1,31 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { Button, SafeAreaView, Text } from 'react-native';
+import { Button, SafeAreaView, Text, View } from 'react-native';
 
 function App() {
-  const [number, setNumber] = useState(0);
-  const [counter, setCounter] = useState(100);
+  const [helloFlag, setHelloFlag] = useState(true);
 
-  useEffect(() => {
-    console.log('number updated.. ');
-  }, [number]);
-
-  useEffect(() => {
-    console.log('counter updated.. ');
-  }, [counter]);
-
-  useEffect(() => {
-    console.log('Mounting..');
-  },[]);
-
+  function updateFlag() {
+    setHelloFlag(!helloFlag);
+  }
   return (
     <SafeAreaView>
       <Text>Hello Lifecycle</Text>
-      <Text>Number {number}</Text>
-      <Text>Counter {counter}</Text>
-      <Button title='Up!' onPress={() => setNumber(number + 1)} />
-      <Button title='Update Counter!' onPress={() => setCounter(counter + 100)} />
+      <Button title='Up!' onPress={updateFlag} />
+      {helloFlag && <Hello />}
     </SafeAreaView>
   )
 }
 
 export default App;
+
+function Hello() {
+  useEffect(() => {
+    console.log('Hello World!');
+
+    return () => {
+      console.log('Gidiyorum bütün aşklar yüreğimde..');
+    };
+  }, []);
+  return(
+    <View style={{backgroundColor: 'green', padding: 10}}>
+      <Text>I'm Hello Component</Text>
+    </View>
+  );
+}
