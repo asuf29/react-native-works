0b50934b2db32a5e7e5d9c738380c4aeab1702aa
asuf29 <asuf29@icloud.com>
2023-12-11 13:51:08 +0300

    Filtering process added

--- MusicApp/src/App.js
+++ MusicApp/src/App.js
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { SafeAreaView, Text, View, FlatList, StyleSheet } from 'react-native';
 import music_data from './music-data.json';
 import SongCard from './components/SongCard';
 import SearchBar from './components/SearchBar';
 
 function App() {
+  const [list, setList] = useState(music_data);
   const renderSong = ({item}) => <SongCard song={item} />
   const renderSeparator = () => <View style={styles.separator}/>
-  const handleSearch = text => console.log(text); 
+  const handleSearch = text => {
+    const filteredList = music_data.filter(song => {
+      const searchedText = text.toLowerCase();
+      const currentTitle = song.title.toLowerCase();
+      
+      return currentTitle.indexOf(searchedText) > -1;
+    });
+    setList(filteredList);
+  }; 
 
   return (
     <SafeAreaView style={styles.container}>
         <SearchBar onSearch={handleSearch}/>
         <FlatList 
           keyExtractor={item => item.id}
-          data={music_data}
+          data={list}
           renderItem={renderSong}
           ItemSeparatorComponent={renderSeparator}
         /> 
