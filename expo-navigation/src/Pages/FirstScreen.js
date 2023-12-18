c1c7b55e66e4d796abc73ca5c6862740b96cbcf8
asuf29 <asuf29@icloud.com>
2023-12-18 14:52:26 +0300

    Added transition between pages

--- expo-navigation/src/Pages/FirstScreen.js
+++ expo-navigation/src/Pages/FirstScreen.js
@@ -1,13 +1,32 @@
 import React from 'react'
-import { SafeAreaView, Text, Button } from 'react-native'
+import { SafeAreaView, Text, Button, StyleSheet} from 'react-native'
 
-function FirstScreen() {
+function FirstScreen(props) {
+  function navigateToPage() {
+    props.navigation.navigate('SecondScreen') 
+  }
+  
   return (
-    <SafeAreaView>
-      <Text>FirstScreen</Text>
-      <Button title='Go To Second Screen' onPress={() => null}/>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text_input}>FirstScreen</Text>
+      <Button title='Go To Second Screen' onPress={navigateToPage}/>
     </SafeAreaView>
   )
 }
 
 export default FirstScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text_input: {
+    borderWidth: 2,
+    borderRadius: 5,
+    borderColor: 'black',
+    margin: 10,
+    padding: 10,
+  },
+})
--- expo-navigation/src/Pages/SecondScreen.js
+++ expo-navigation/src/Pages/SecondScreen.js
@@ -1,12 +1,32 @@
 import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { SafeAreaView, Text, StyleSheet, Button} from 'react-native'
 
-function SecondScreen() {
+function SecondScreen(props) {
+  function navigateToPage() {
+    props.navigation.navigate('FirstScreen') 
+  }
+
   return (
-    <SafeAreaView>
-      <Text>FirstScreen</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text_input}>Second Screen</Text>
+      <Button title='Go To First Screen' onPress={navigateToPage}/>
     </SafeAreaView>
   )
 }
 
 export default SecondScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text_input: {
+    borderWidth: 2,
+    borderRadius: 5,
+    borderColor: 'black',
+    margin: 10,
+    padding: 10,
+  },
+})
