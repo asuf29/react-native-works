f9fa976ea83462a76a766ae31f9e5a87320b63d7
asuf29 <asuf29@icloud.com>
2023-12-19 15:06:07 +0300

    button styled

---
+++ kebap-fitness-app/src/components/Button/Button.style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: '#1976d2',
+    borderRadius: 5,
+    padding: 10,
+    margin: 5,
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
