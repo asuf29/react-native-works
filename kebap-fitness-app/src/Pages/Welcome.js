18978a6d3dd0ef93f71e78a3f951404eb77631e6
asuf29 <asuf29@icloud.com>
2023-12-19 15:10:08 +0300

    button has been imported

--- kebap-fitness-app/src/Pages/Welcome.js
+++ kebap-fitness-app/src/Pages/Welcome.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { SafeAreaView , Text, View} from 'react-native'
+import Button from '../components/Button/Button';
 
 function Welcome() {
   return(
     <SafeAreaView>
       <Text>Welcome</Text>
+      <Button text="Üye Kaydı Oluştur" onPress={null}/>
     </SafeAreaView>
   );
 }
