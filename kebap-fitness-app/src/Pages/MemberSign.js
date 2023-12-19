dc853954f29d08cb1511716b4b1eed5cf10cacea
asuf29 <asuf29@icloud.com>
2023-12-19 16:49:48 +0300

    input is styled

--- kebap-fitness-app/src/Pages/MemberSign.js
+++ kebap-fitness-app/src/Pages/MemberSign.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
+import Input from '../components/Input/Input';
 
 function MemberSign() {
   return(
     <SafeAreaView>
       <Text>MemberSign</Text>
+      <Input label="Üye Adı"/>
     </SafeAreaView>
   );
 }
---
+++ kebap-fitness-app/src/components/Input/Input.style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  input_container: {
+    borderWidth: 1,
+    margin: 10,
+    padding: 10,
+    borderRadius: 5,
+    borderColor: '#bdbdbd',
+  },
+  label: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginLeft: 10,
+  }
+});
