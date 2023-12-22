efa1ed44f79117a4c4149dbeedf7f1b2812fea29
asuf29 <asuf29@icloud.com>
2023-12-22 11:37:18 +0300

    user information received

--- kebap-fitness-app/src/Pages/MemberSign.js
+++ kebap-fitness-app/src/Pages/MemberSign.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native'
 import Input from '../components/Input/Input';
 import Button from '../components/Button'
 
 function MemberSign() {
-  return(
+  const [userName, setUserName] = useState('');
+  const [userSurname, setUserSurname] = useState('');
+  const [userAge, setUserAge] = useState('');
+  const [userMail, setUserMail] = useState('');
+
+  function handleSubmit() {
+    const user = {
+      userName,
+      userSurname,
+      userAge,
+      userMail,
+    };
+    console.log(user);
+  }
+
+   return(
     <SafeAreaView>
-      <Input label="Üye Adı" placeholder="Üyenin ismini giriniz..."/>
-      <Input label="Üye Soyadı" placeholder="Üyenin soyadını giriniz..."/>
-      <Input label="Üye Yaşı" placeholder="Üyenin yaşını giriniz..."/>
-      <Input label="Üye E-mail" placeholder="Üyenin e-mail adresini giriniz..."/>
-      <Button text="Kaydı Tamamla" onPress={null}/>
+      <Input label="Üye Adı" placeholder="Üyenin ismini giriniz..." onChangeText={setUserName} />
+      <Input label="Üye Soyadı" placeholder="Üyenin soyadını giriniz..." onChangeText={setUserSurname} />
+      <Input label="Üye Yaşı" placeholder="Üyenin yaşını giriniz..." onChangeText={setUserAge} />
+      <Input label="Üye E-mail" placeholder="Üyenin e-mail adresini giriniz..." onChangeText={setUserMail} />
+      <Button text="Kaydı Tamamla" onPress={handleSubmit}/>
     </SafeAreaView>
   );
 }
